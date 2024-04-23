ddd9b2fe3579104c86bf34085ec81530e9b6393e
Maurice Busch <gherrink@gmail.com>
2024-04-23 09:33:48 +0200

    style(demos): applyed new package structure

--- demos/base.ts
+++ demos/base.ts
@@ -1,10 +1,38 @@
 import { Styleguide } from '@styleguide/core'
-import { BaseFileFinder } from 'packages/core/src/BaseFileFinder'
+import { HtmlRenderer } from '@styleguide/html-renderer'
+import { NodeFileFinder, NodeFileReader, NodeFileWriter } from '@styleguide/node'
+import { BaseSourceListener } from 'packages/core/src/BaseSourceListener'
+import path from 'path'
 
-const styleguide: Styleguide = new Styleguide({
-  finder: new BaseFileFinder({
-    globs: ['less/**/*.less'],
-  }),
+// TODO fix imports
+
+const finder = new NodeFileFinder()
+const writer = new NodeFileWriter('./dist/base')
+const reader = new NodeFileReader()
+const listener = new BaseSourceListener({
+  globs: ['less/**/*.less'],
+}, finder)
+const renderer = new HtmlRenderer()
+
+const templatesPath = path.resolve('../packages/html-renderer/templates')
+
+finder.scan([`${templatesPath}/layouts/*.html`], file => {
+  renderer.addLayout(path.basename(file, path.extname(file)), reader.content(file).trim())
 })
 
-styleguide.load()
+finder.scan([`${templatesPath}/pages/*.html`], file => {
+  renderer.addPage(path.basename(file, path.extname(file)), reader.content(file).trim())
+})
+
+finder.scan([`${templatesPath}/partials/*.html`], file => {
+  renderer.addPartial(path.basename(file, path.extname(file)), reader.content(file).trim())
+})
+
+const styleguide: Styleguide = new Styleguide({
+  reader,
+  writer,
+  listener,
+  renderer,
+})
+
+styleguide.output()
