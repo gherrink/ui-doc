6879ab9b55ecdff2ea5ffcae3218002a9bd7bb53
Maurice Busch <gherrink@gmail.com>
2024-06-22 07:45:45 +0200

    build(demos): changed rollup styleguide settings

--- demos/rollup.config.mjs
+++ demos/rollup.config.mjs
@@ -1,5 +1,6 @@
 import styleguide from '@styleguide/rollup'
 import autoprefixer from 'autoprefixer'
+import postcssExtend from 'postcss-extend'
 import postcssImport from 'postcss-import'
 import postcssNested from 'postcss-nested'
 import postcss from 'rollup-plugin-postcss'
@@ -7,7 +8,7 @@ import postcss from 'rollup-plugin-postcss'
 export default [
   {
     input: {
-      app: 'css/app.css',
+      app: 'css/index.css',
     },
     output: {
       dir: 'dist/rollup',
@@ -15,62 +16,23 @@ export default [
       sourcemap: true,
     },
     plugins: [
-      // resolve(),
-      // commonjs(),
       postcss({
         extract: 'app.css',
         minimize: true,
-        plugins: [postcssImport(), postcssNested(), autoprefixer()],
+        plugins: [postcssImport(), postcssNested(), postcssExtend(), autoprefixer()],
         sourceMap: true,
       }),
       styleguide({
+        settings: {
+          generate: {
+            logo: () => 'Rollup',
+          },
+          texts: {
+            title: 'Rollup Test',
+          },
+        },
         source: ['css/**/*.css'],
-        styleAsset: false,
       }),
     ],
   },
-  // {
-  //   input: '../packages/html-renderer/styles/index.css',
-  //   output: {
-  //     dir: 'dist/rollup',
-  //     format: 'es',
-  //     name: 'styleguide',
-  //     sourcemap: true,
-  //   },
-  //   plugins: [
-  //     postcss({
-  //       extract: 'styleguide.css',
-  //       minimize: true,
-  //       plugins: [postcssImport(), postcssNested(), postcssExtend(), autoprefixer()],
-  //       sourceMap: true,
-  //     }),
-  //   ],
-  // },
-  // {
-  //   external: [],
-  //   input: { styleguide: '../packages/html-renderer/scripts/styleguide.ts' },
-  //   output: [
-  //     {
-  //       dir: 'dist/rollup',
-  //       format: 'umd',
-  //       sourcemap: false,
-  //     },
-  //   ],
-  //   plugins: [
-  //     resolve(),
-  //     commonjs(),
-  //     babel({
-  //       babelHelpers: 'bundled',
-  //       exclude: '../node_modules/**',
-  //     }),
-  //     typescript({
-  //       tsconfig: '../tsconfig.web.json',
-  //       tsconfigOverride: {
-  //         compilerOptions: {
-  //           declaration: true,
-  //         },
-  //       },
-  //     }),
-  //   ],
-  // },
 ]
